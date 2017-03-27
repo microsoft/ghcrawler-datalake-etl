4cc3d38e729d10b851cb6744fd06016229a000e6
Doug Mahugh <dmahugh@microsoft.com>
2017-03-27 15:16:48 -0700

    implement setting()

--- verification/ghiverify.py
+++ verification/ghiverify.py
@@ -5,21 +5,20 @@ Licensed under the MIT License.
 ghiverify.py - tools to verify data integrity for GHInsights data
 """
 import collections
-import configparser
 import csv
 import datetime
 import glob
 import gzip
 import json
-import os
 import re
-import sys
 from timeit import default_timer
 
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.datalake.store import DataLakeStoreAccountManagementClient
 from azure.datalake.store import core, lib, multithread
 import requests
+
+from dougerino import days_since, dicts2csv, filesize, setting
 
 #----------------------------------------------------------------------------<<<
 # MISCELLANEOUS                                                              <<<
@@ -66,7 +65,7 @@ def daily_diff(): #----------------------------------------------------------<<<
     start_time = default_timer()
     print_log(10*'-' + ' Data Verification for ' + datestr + ' ' + 10*'-')
 
-    for entity in ['repo']:
+    for entity in ['Repo']:
         entity_start = default_timer()
 
         # download the current CSV file from Azure Data Lake Store
@@ -143,12 +142,6 @@ def date_range(datestr): #---------------------------------------------------<<<
         return '365'
     return 'older'
 
-def days_since(datestr): #---------------------------------------------------<<<
-    """Return # days since a date in YYYY-MM-DD format.
-    """
-    return (datetime.datetime.today() -
-            datetime.datetime.strptime(datestr, '%Y-%m-%d')).days
-
 def diff_report(entity=None, masterfile=None, comparefile=None): #-----------<<<
     """Generate a diff report for specified entity.
 
@@ -182,11 +175,6 @@ def documentation_repo(reponame): #------------------------------------------<<<
             return True
 
     return False
-
-def filesize(filename): #----------------------------------------------------<<<
-    """Return byte size of specified file.
-    """
-    return os.stat(filename).st_size
 
 def local_filename(entity, source): #----------------------------------------<<<
     """Return filename of a local data file.
@@ -440,25 +428,6 @@ def repo_execvp_voting(): #--------------------------------------------------<<<
         open(outfile, 'a').write(org_repo.replace('/', ',') +
                                  ',' + execvp + '\n')
 
-def setting(topic=None, section=None, key=None): #---------------------------<<<
-    """Retrieve a private setting stored in a local .ini file.
-
-    topic = name of the ini file; e.g., 'azure' for azure.ini
-    section = section within the .ini file
-    key = name of the key within the section
-
-    Returns the value if found, None otherwise.
-    """
-    source_folder = os.path.dirname(os.path.realpath(__file__))
-    inifile = os.path.join(source_folder, '../_private/' + topic.lower() + '.ini')
-    config = configparser.ConfigParser()
-    config.read(inifile)
-    try:
-        retval = config.get(section, key)
-    except configparser.NoSectionError:
-        retval = None
-    return retval
-
 def token_creds(): #---------------------------------------------------------<<<
     """Return token and credentials for Azure Active Directory authentication.
     """
@@ -472,27 +441,6 @@ def token_creds(): #---------------------------------------------------------<<<
         ServicePrincipalCredentials(client_id=clientid,
                                     secret=clientsecret,
                                     tenant=tenantid))
-
-def write_csv(listobj, filename): #------------------------------------------<<<
-    """Write list of dictionaries to a CSV file.
-
-    1st parameter = the list of dictionaries
-    2nd parameter = name of CSV file to be written
-    """
-    csvfile = open(filename, 'w', newline='')
-
-    # note that we assume all dictionaries in the list have the same keys
-    csvwriter = csv.writer(csvfile, dialect='excel')
-    header_row = [key for key, _ in listobj[0].items()]
-    csvwriter.writerow(header_row)
-
-    for row in listobj:
-        values = []
-        for fldname in header_row:
-            values.append(row[fldname])
-        csvwriter.writerow(values)
-
-    csvfile.close()
 
 #----------------------------------------------------------------------------<<<
 # AZURE                                                                      <<<
@@ -822,7 +770,7 @@ def github_get_repos(): #----------------------------------------------------<<<
     templist = github_data(endpoint='/user/orgs', fields=['login'])
     sortedlist = sorted([_['login'].lower() for _ in templist])
     user_orgs = [orgname for orgname in sortedlist
-                 if not orgname == 'nuget']
+                 if not orgname in ['deployr', 'msftberlin', 'nuget']]
 
     repolist = [] # the list of repos
     for orgid in user_orgs:
@@ -831,7 +779,7 @@ def github_get_repos(): #----------------------------------------------------<<<
             fields=['owner.login', 'name', 'created_at']))
 
     sorted_data = sorted(repolist, key=data_sort)
-    write_csv(sorted_data, filename) # write CSV file
+    dicts2csv(sorted_data, filename) # write CSV file
 
 def github_pagination(link_header): #----------------------------------------<<<
     """Parse values from the 'link' HTTP header returned by GitHub API.
@@ -987,8 +935,6 @@ def test_commit_count(): #---------------------------------------------------<<<
     testcases = ['microsoft/dotnet',
                  'microsoft/vscode',
                  'microsoft/typescript',
-                 'microsoft/xaml-standard',
-                 'microsoft/ospo-witness',
                  'microsoft/ghcrawler-datalake-etl']
     for orgrepo in testcases:
         orgname = orgrepo.split('/')[0]
@@ -999,4 +945,4 @@ def test_commit_count(): #---------------------------------------------------<<<
 # code to be executed when running standalone (for ad-hoc testing, etc.)
 if __name__ == '__main__':
     #daily_diff()
-    add_execvp()
+    test_commit_count()
